9ff397692266e8d8938843c1dd6d2c92a8d496be
Goodnour <131546703+Goodnour@users.noreply.github.com>
2023-04-22 18:58:02 +0200

    Add files via upload

---
+++ Project IT/تسجيل الدخول/Untitled-2.js
@@ -0,0 +1,9 @@
+// Get the link and div elements by their IDs
+const myLink = document.getElementById("myLink");
+const myDiv = document.getElementById("myDiv");
+
+// Remove the underline from the link
+myLink.style.textDecoration = "none";
+
+// Set the background color of the div to yellow
+myDiv.style.backgroundColor = "yellow";
